c6463e1e764741e0631f0c6ef8d7aac665da843a
Goodness Iyamah <goodnessiyamah1@gmail.com>
2025-10-04 15:42:42 +0100

    Error caught with return

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [],
+  plugins: [
+    '@babel/plugin-transform-export-namespace-from',
+    'react-native-reanimated/plugin'
+  ],
 };
